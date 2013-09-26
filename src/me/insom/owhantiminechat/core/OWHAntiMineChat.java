1cc7282e9d895a14969a0cd21842b3fe784faadc
Tyler Hackett <ins10102@gmail.com>
2013-09-26 16:46:14 -0700

    First commit (Still need plugin.yml and crap)
    Will add the plugin next, then a timer to check if the player had logged
    on within the last 10 seconds

---
+++ src/me/insom/owhantiminechat/core/OWHAntiMineChat.java
@@ -0,0 +1,29 @@
+package me.insom.owhantiminechat.core;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.player.AsyncPlayerChatEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class OWHAntiMineChat extends JavaPlugin implements org.bukkit.event.Listener{
+	
+	public OWHAntiMineChat(){
+		getServer().getPluginManager().registerEvents(this, this);
+	}
+	
+	String peskyMessageStart = "connected with a";
+	String peskyMessageEnd = "using MineChat";
+	
+	@EventHandler(priority = EventPriority.LOWEST)
+	public void playerChat(AsyncPlayerChatEvent event)
+	{	
+		// Check if this message starts and ends with the horrid MineChat spam. This avoids having to check every single type of device (iphone, ipod, etc..).
+		if(event.getMessage().startsWith(peskyMessageStart) && event.getMessage().endsWith(peskyMessageEnd))
+		{
+			event.setCancelled(true); // No spam today, stupid app!
+		}
+		
+	}
+	
+	
+}
